b7f9da4c95bb4a6947d3d8a8c567dc9feb41f880
leetk77777 <leetk2495@naver.com>
2021-05-13 13:24:56 +0900

    Update HelloWorld.java

--- src/main/java/com/test/boot/HelloWorld.java
+++ src/main/java/com/test/boot/HelloWorld.java
@@ -8,6 +8,7 @@ public class HelloWorld {
 		System.out.println("hello world3");
 		System.out.println("hello world5");
 		System.out.println("hello world7");
+		System.out.println("hello world8");
 	}
 
 }
